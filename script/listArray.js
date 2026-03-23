1cf238d3b60d81d6cb6c4531c59761fdd597753d
최희정 <hee.j0945@gmail.com>
2026-03-23 18:05:55 +0900

    260323

---
+++ script/listArray.js
@@ -0,0 +1,99 @@
+let listArray = [
+    {
+        pid:0,
+        pName:"잠봉뵈르",
+        pDesc:"수제 잠봉 햄과 프랑스산 버터로 완성한 클래식한 조화",
+        pPrice: 7800,
+        pThumbnail: "list-thumb-Jambon.jpg",
+        plike:false
+    },
+    {
+        pid:1,
+        pName:"아보카도 에그",
+        pDesc:"크리미한 아보카도와 반숙 에그가 어우러진 부드러운 식감",
+        pPrice: 5500,
+        pThumbnail: "list-thumb-egg.jpg",
+        plike:false
+    },
+    {
+        pid:2,
+        pName:"치킨 페스토",
+        pDesc:"허브 페스토와 촉촉한 치킨으로 산뜻하게 즐기는 풍미",
+        pPrice: 6500,
+        pThumbnail: "list-thumb-Chicken.jpg",
+        plike:false
+    },
+    {
+        pid:3,
+        pName:"발사믹 머쉬룸 리코타",
+        pDesc:"발사믹 머쉬룸과 리코타 치즈의 깊고 고소한 풍미",
+        pPrice: 6500,
+        pThumbnail: "list-thumb-Mushroom.jpg",
+        plike:false
+    },
+    {
+        pid:4,
+        pName:"브리 무화과 발사믹",
+        pDesc:"브리 치즈와 무화과, 발사믹의 달콤한 조화",
+        pPrice: 6900,
+        pThumbnail: "list-thumb-Brie.jpg",
+        plike:false
+    },
+    {
+        pid:5,
+        pName:"레몬 로스트 치킨",
+        pDesc:"레몬 크림 소스와 로스트 치킨의 상큼한 밸런스",
+        pPrice: 6500,
+        pThumbnail: "list-thumb-Lemon.jpg",
+        plike:false
+    },
+    {
+        pid:6,
+        pName:"허니 리코타 애플",
+        pDesc:"꿀과 리코타, 신선한 사과를 더한 산뜻한 밸런스",
+        pPrice: 6900,
+        pThumbnail: "list-thumb-Apple.jpg",
+        plike:false
+    },
+    {
+        pid:7,
+        pName:"쉬림프 갈릭 바질",
+        pDesc:"갈릭버터 새우에 바질 향을 더한 깊은 맛",
+        pPrice: 7800,
+        pThumbnail: "list-thumb-Shrimp.jpg",
+        plike:false
+    },
+    {
+        pid:8,
+        pName:"연어 딜 크림치즈",
+        pDesc:"훈제연어와 딜 크림치즈의 상큼한 레이어",
+        pPrice: 7800,
+        pThumbnail: "list-thumb-Salmon.jpg",
+        plike:false
+    },
+    {
+        pid:9,
+        pName:"리얼 비프 샌드",
+        pDesc:"풍성한 비프에 치즈를 더한 진한 만족감",
+        pPrice: 8500,
+        pThumbnail: "list-thumb-Beef.jpg",
+        plike:false
+    },
+    {
+        pid:10,
+        pName:"내슈빌 치킨",
+        pDesc:"바삭한 치킨에 매콤한 소스를 더한 강렬한 맛",
+        pPrice: 8500,
+        pThumbnail: "list-thumb-Nashville.jpg",
+        plike:false
+    },
+    {
+        pid:11,
+        pName:"크리미 에그 포테이토",
+        pDesc:"부드러운 에그와 감자가 어우러진 든든한 구성",
+        pPrice: 5500,
+        pThumbnail: "list-thumb-Potato.jpg",
+        plike:false
+    },
+
+]
